153f34a8fbfb4052d5e96e515ca5764240a653c0
MasterWinZ <supawit.promma6256@gmail.com>
2022-10-02 21:26:55 +0700

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+  VANTA.WAVES({
+    el: "#vanta",
+    mouseControls: false,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x1e1e1e,
+  })
+})
